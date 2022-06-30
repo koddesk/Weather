0701e008ebf90fc2da722f3b2cebdf48bc24d62d
Nikolay Dobychin <imkoddi@mail.ru>
2022-06-30 19:37:40 +0300

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by KODDER on 30.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
