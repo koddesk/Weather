e8954e2fb5f62acc4ecb31d70a6725c82906c98c
Nikolay Dobychin <imkoddi@mail.ru>
2022-07-01 12:46:45 +0300

    Custom Tab Bar

--- Weather/Views/Main/HomeView.swift
+++ Weather/Views/Main/HomeView.swift
@@ -9,35 +9,41 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        ZStack {
-            //MARK: Background Color
-            Color.background
-                .ignoresSafeArea()
-            
-            //MARK: Background Color
-            Image("Background")
-                .resizable()
-                .ignoresSafeArea()
-            
-            //MARK: House Image
-            Image("House")
-                .frame(maxHeight: .infinity, alignment: .top)
-                .padding(.top, 257)
-            
-            VStack(spacing: -10) {
-                Text("Saint-Petersburg")
-                    .font(.largeTitle)
+        NavigationView {
+            ZStack {
+                //MARK: Background Color
+                Color.background
+                    .ignoresSafeArea()
                 
-                VStack {
-                    Text(attributedString)
+                //MARK: Background Color
+                Image("Background")
+                    .resizable()
+                    .ignoresSafeArea()
+                
+                //MARK: House Image
+                Image("House")
+                    .frame(maxHeight: .infinity, alignment: .top)
+                    .padding(.top, 257)
+                
+                VStack(spacing: -10) {
+                    Text("Saint-Petersburg")
+                        .font(.largeTitle)
                     
-                    Text("H:24°   L:18°")
-                        .font(.title3.weight(.semibold))
+                    VStack {
+                        Text(attributedString)
+                        
+                        Text("H:24°   L:18°")
+                            .font(.title3.weight(.semibold))
+                    }
+                    
+                    Spacer()
                 }
+                .padding(.top, 51)
                 
-                Spacer()
+                //MARK: Tab Bar
+                TabBar (action: {})
             }
-            .padding(.top, 51)
+            .navigationBarHidden(true)
         }
     }
     
